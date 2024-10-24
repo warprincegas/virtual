aaa82ca906128de8767b4753f432c213a648425c
WarDudeFoxes <153716884+WarDudeFoxes@users.noreply.github.com>
2024-10-24 21:19:58 +0100

    fiasjhjt

--- main.js
+++ main.js
@@ -1,226 +1,32 @@
-import * as THREE from "three";
+import Home from "./pages/home.jsx";
+import About from "./pages/about.jsx";
+import Contact from "./pages/contact.jsx";
 import "./style.css";
-import gsap from "gsap";
-import { events } from "./Modules/eventsListeners.js";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-let camera, scene, renderer, controls, raycaster, mouse;
-let points = [];
-
-const pointLabels = [
-  "Entrance",
-  "Main Hall",
-  "Left Wing",
-  "Right Wing",
-  "Back Door",
-];
-
-const pointImages = [
-  "Abuja Central Mosgue.jpeg",
-  "Abuja Central Mosgue.jpeg",
-  "Mpape-Crushed-rock.jpeg",
-  "Abuja Central Mosgue.jpeg",
-];
-
-const spinner = document.querySelector(".loader");
-
-// Create a loading manager
-const loadingManager = new THREE.LoadingManager();
-
-// When all resources are loaded, hide the spinner
-loadingManager.onLoad = function () {
-  spinner.classList.add("hidden");
+// Define routes
+export const routes = {
+  "/": Home,
+  "/about": About,
+  "/contact": Contact,
 };
 
-// Optional: You can also listen for progress updates
-loadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
-  console.log(`Loaded ${itemsLoaded} of ${itemsTotal} files`);
-};
+console.log(About);
 
-// // Optional: Handle loading error
-// loadingManager.onError = function (url) {
-//   console.error('There was an error loading:', url);
-// };
-
-// function pageScene() {
-const container = document.querySelector(".image");
-document.body.appendChild(container);
-
-scene = new THREE.Scene();
-
-camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  1,
-  1000
-);
-camera.position.set(0, 0, 0.1);
-
-renderer = new THREE.WebGLRenderer({ antialias: false }); // Disabled anti-aliasing for performance
-renderer.setPixelRatio(window.devicePixelRatio / 2);
-renderer.setSize(window.innerWidth, window.innerHeight);
-container.appendChild(renderer.domElement);
-
-const textureLoader = new THREE.TextureLoader(loadingManager);
-const texture = textureLoader.load("Abuja Central Mosgue.jpeg");
-
-const geometry = new THREE.SphereGeometry(500, 60, 40);
-geometry.scale(-1, 1, 1);
-
-const material = new THREE.MeshBasicMaterial({
-  map: texture,
-});
-const sphere = new THREE.Mesh(geometry, material);
-
-const group = new THREE.Group();
-group.add(sphere);
-scene.add(group);
-
-controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.enableZoom = true;
-controls.dampingFactor = 0.05;
-controls.screenSpacePanning = true;
-// controls.autoRotate = true;
-// events(controls);
-
-raycaster = new THREE.Raycaster();
-mouse = new THREE.Vector2();
-
-function createImagePoint(X, Y, Z, label, imagePath) {
-  const textureLoader = new THREE.TextureLoader();
-  const texture = textureLoader.load(imagePath);
-
-  const geometry = new THREE.CircleGeometry(0.3, 32);
-  const material = new THREE.MeshBasicMaterial({
-    map: texture,
-    // transparent: true,
-  });
-
-  const plane = new THREE.Mesh(geometry, material);
-  plane.position.set(X, Y, Z);
-  plane.lookAt(camera.position);
-  group.add(plane);
-  points.push(plane);
-
-  plane.userData.label = label;
-}
-createImagePoint(-2.75, -1.24, -2.77, pointLabels[0], pointImages[0]);
-createImagePoint(0.73, 1.35, 4.88, pointLabels[1], pointImages[1]);
-createImagePoint(-3.12, 0.22, 3.13, pointLabels[2], pointImages[2]);
-createImagePoint(-2.13, 1.56, -4.64, pointLabels[3], pointImages[3]);
-
-function adjustPointScale() {
-  const cameraPosition = new THREE.Vector3();
-  camera.getWorldPosition(cameraPosition);
-
-  points.forEach((point) => {
-    const distance = cameraPosition.distanceTo(point.position);
-
-    const scale = THREE.MathUtils.clamp((1 / distance) * 5, 0.1, 2);
-    point.scale.set(scale, scale, scale);
-  });
+// Function to navigate and load the correct page
+function navigate() {
+  const path = location.hash.slice(1) || "/"; // Get path from hash (default to home '/')
+  const page = routes[path] || Home; // Default to Home if path is not found
+  document.getElementById("app").innerHTML = page();
+  console.log(document.querySelector("body"));
+  // Render the page
 }
 
-function animate() {
-  requestAnimationFrame(animate);
+// document.querySelector(".contact-link").addEventListener("click", () => {
+//   document.querySelector(".contact-link").setAttribute("href", "#/contact");
+// });
 
-  raycaster.setFromCamera(mouse, camera);
+// Event listener for hash changes
+window.addEventListener("hashchange", navigate);
 
-  const intersects = raycaster.intersectObjects(points);
-
-  points.forEach((point) => {
-    point.scale.set(1, 1, 1);
-  });
-  document.body.style.cursor = "grab";
-  const contentElement = document.getElementById("hover-content");
-  adjustPointScale();
-
-  if (intersects.length > 0) {
-    const intersectedPoint = intersects[0].object;
-
-    gsap.to(intersectedPoint.scale, { x: 1.5, y: 1.5, duration: 0.5 }); // Scale up on hover
-
-    displayContent(intersectedPoint.userData.label);
-
-    document.body.style.cursor = "pointer";
-  } else {
-    contentElement.style.display = "none";
-  }
-
-  controls.update();
-  renderer.render(scene, camera);
-}
-
-animate();
-events(controls, mouse, camera, renderer, raycaster);
-
-function displayContent(label) {
-  const contentElement = document.getElementById("hover-content");
-  contentElement.style.display = "block";
-  contentElement.innerText = `Point: ${label}`;
-
-  const { width, height } = contentElement.getBoundingClientRect();
-
-  contentElement.style.left = `${
-    (mouse.x * window.innerWidth) / 2 + window.innerWidth / 2 - width / 2
-  }px`;
-  contentElement.style.top = `${
-    (-mouse.y * window.innerHeight) / 2 + window.innerHeight / 2 - height / 2
-  }px`;
-}
-
-function onTouchStart(event) {
-  // Convert touch coordinates to the same normalized values used for mouse hover
-  const touch = event.touches[0];
-  mouse.x = (touch.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(touch.clientY / window.innerHeight) * 2 + 1;
-  controls.enableZoom = false;
-
-  // Trigger the same behavior as hover when touching
-  handleInteraction();
-}
-
-function onTouchEnd() {
-  // Simulate hover end by hiding content or resetting the point image
-  const contentElement = document.getElementById("hover-content");
-  contentElement.style.display = "none";
-
-  // Reset point scales
-  points.forEach((point) => {
-    gsap.to(point.scale, { x: 1, y: 1, duration: 0.5 });
-  });
-}
-
-function handleInteraction() {
-  // Update raycaster with camera and mouse/touch position
-  raycaster.setFromCamera(mouse, camera);
-
-  // Calculate objects intersecting with the ray
-  const intersects = raycaster.intersectObjects(points);
-
-  // Reset point scales and content
-  points.forEach((point) => {
-    gsap.to(point.scale, { x: 1, y: 1, duration: 0.5 });
-  });
-  const contentElement = document.getElementById("hover-content");
-  contentElement.style.display = "none"; // Hide content initially
-
-  // If an intersection is detected, simulate hover effect
-  if (intersects.length > 0) {
-    const intersectedPoint = intersects[0].object;
-
-    // Scale up the intersected point
-    gsap.to(intersectedPoint.scale, { x: 1.5, y: 1.5, duration: 0.5 });
-
-    // Display content for the intersected point
-    displayContent(intersectedPoint.userData.label);
-  }
-}
-
-// Add the touch event listeners
-window.addEventListener("touchstart", onTouchStart);
-window.addEventListener("touchend", onTouchEnd);
-// }
-
-//Loader Function
+// Load the initial route
+window.addEventListener("load", navigate);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
       sm: "480px",
